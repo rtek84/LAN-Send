d35ac01be1f24371e760dc8dd9b87eb1cb5dfd66
rtek84 <rtek84@gmail.com>
2026-09-04 23:03:24 +0800

    Point LAN Send launcher to renamed receiver

--- PC/Start-LAN-Send.cmd
+++ PC/Start-LAN-Send.cmd
@@ -1,2 +1,2 @@
 @echo off
-start "LAN Send" powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0PocketDrop-Receiver.ps1"
+start "LAN Send" powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0LANSend-Receiver.ps1"
