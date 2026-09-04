8da32044c79412fd115fa2ba8d4a1f12ebe18078
rtek84 <rtek84@gmail.com>
2026-09-04 11:45:26 +0800

    Add phone receiver dependencies and bump PocketDrop 3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.raytek.pocketdrop"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
     }
 }
 
